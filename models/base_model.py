123c0296222d3b002bdb99085f6771b556e0758d
Gerson Antonio <gelsonantonio53@outlook.com>
2023-10-16 10:28:18 +0200

    pycodestyle complient

--- models/base_model.py
+++ models/base_model.py
@@ -3,7 +3,7 @@
 This is the base or parent class for the Air B&B project, all the other classes
 will inharit from this class, it is the main blueprint of the application
 
-We use the UUID an the DATETIME packages, for creating a unique ID 
+We use the UUID an the DATETIME packages, for creating a unique ID
 and date and time respectively
 
 This model will be used to manipulate the console
@@ -13,18 +13,17 @@ import uuid
 from datetime import datetime
 # from models import storage
 
+
 class BaseModel():
     """
     The base class for the air B&B project
-    
+
     attributes:
         id (str): the unique ID attributed to each object created
         created_at (str): the date and time of the object created
         updated_at (str): the date and time of the object's last modification
-        
     """
-    
-    
+
     def __init__(self, *args, **kwargs):
         """
         Initialization of the base class
@@ -44,20 +43,18 @@ class BaseModel():
                     setattr(self, key, datetime.fromisoformat(value))
                 else:
                     setattr(self, key, value)
-        
+
     def save(self):
         """
         update the updated_at attribute
         """
         # storage.save()
         self.updated_at = datetime.now()
-    
-        
-    
+
     def to_dict(self):
         """
         takes a python and converts it to a dictionary
-        
+
         return:
             returns the dictionary representation of the object
         """
@@ -66,16 +63,12 @@ class BaseModel():
         self.__dict__["created_at"] = self.created_at.isoformat()
         self.__dict__["id"] = str(self.id)
         return self.__dict__
-        
-    
+
     def __str__(self):
         """
         the string representation of the object
-        
+
         returns:
             the string representation of the BaseModel
         """
-        return "[{}] ({}) {}".format(type(self).__name__, self.id, self.__dict__)
-    
-    
-    # code ends here _______________________________________________
+        return f"[{type(self).__name__}] ({self.id}) {self.__dict__}"
